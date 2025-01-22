34ab05cc11f4ccd384acc130669ddd494812fddb
rohits21381 <rohit.shinde9509@gmail.com>
2025-01-22 13:18:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-0df8c184d5f6ae949"
+    ami = "ami-0d2614eafc1b0e4d2"
     instance_type = "t2.nano"
     tags = {
       Name = "test"
